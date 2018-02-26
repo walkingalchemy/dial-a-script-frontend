1797e59681d257453d46ddf3d265f620c5a27775
walkingalchemy <walkingalchemy@gmail.com>
2018-02-26 11:13:27 -0500

    Import dependencies for redux/thunk/router and setup initial reducer/store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+// import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { BrowserRouter as Router } from 'react-router-dom'
+// import local files
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import usersReducer from './reducers/usersReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+// hook up redux
+const rootReducer = combineReducers({ usersReducer })
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+
---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,10 @@
+export default function usersReducer(state = { username: null }, action) {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      localStorage.setItem('jwt', action.payload.jwt)
+      console.log({...state, username: action.payload.username});
+      return {...state, username: action.payload.username}
+    default:
+      return state
+  }
+}
